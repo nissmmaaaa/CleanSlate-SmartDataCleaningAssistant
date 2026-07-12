f60fa37f778139177781dccce4be793032512e9d
nissmmaaaa <nafeesnismarabi@gmail.com>
2026-07-12 21:44:02 +0530

    Initial Project setup

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_title="CleanSlate", page_icon="🧹", layout="wide")
+st.title("CleanSlate")
+st.subheader("Transform your data effortlessly with CleanSlate! 🧹")
+st.write("""CleanSlate is a powerful data cleaning tool that helps you prepare your datasets for analysis. With just a few clicks, you can remove duplicates, handle missing values, and standardize your data formats. Whether you're a data scientist, analyst, or enthusiast, CleanSlate makes data cleaning easy and efficient.""")
